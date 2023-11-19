e9d337edf089f98cab4f860f86d74f3b786a8784
wonsun2006 <44808210+wonsun2006@users.noreply.github.com>
2023-11-19 10:26:04 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/keywordcrawling/KeywordCrawlingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.keywordcrawling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeywordCrawlingApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeywordCrawlingApplication>(*args)
+}
